1253f0fee8067b2cbbd21888f68436ddc85b021a
Tad Thorley <phaedryx@gmail.com>
2010-09-23 21:17:57 -0600

    thinking about the triangle assignment again has expanded my awareness

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  case [a,b,c].uniq.size
+  sides = [a,b,c]
+  
+  raise TriangleError if sides.find{|s| s <= 0}
+  raise TriangleError if a >= b + c || b >= a + c || c >= a + b
+  
+  case sides.uniq.size
     when 1 then :equilateral
     when 2 then :isosceles
     when 3 then :scalene
