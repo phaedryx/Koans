d53756e76365dbd64987eab794d48a89061a7c45
Tad Thorley <phaedryx@gmail.com>
2010-09-23 21:00:10 -0600

    thinking about the triangle assignment has expanded my awareness

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  case [a,b,c].uniq.size
+    when 1 then :equilateral
+    when 2 then :isosceles
+    when 3 then :scalene
+    else :unknown
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
